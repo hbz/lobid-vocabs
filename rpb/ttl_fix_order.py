bbff2e03ecf61fab7e63be0f246391075dcbb48b
Petra Maier <petra.maier@hbz-nrw.de>
2024-08-14 13:35:39 +0200

    Adjust writing of the output file #rpb-80
    Fix input file path

--- rpb/ttl_fix_order.py
+++ rpb/ttl_fix_order.py
@@ -6,14 +6,13 @@ def format_list_items(unformatted_list):
     return formatted_list
 
 
-with open("/rpb-spatial.ttl", "r") as f:
-#with open("rpb-spatial_final.ttl", "r") as f:
+with open("rpb-spatial.ttl", "r") as f:
     data = f.read()
     test_search = re.findall(r'^:n.*? \.$', data, flags=re.MULTILINE | re.DOTALL)
     prefix_matches = re.findall(r'^@prefix.+\.$\n', data, flags=re.MULTILINE)
     other_matches = re.findall(r'^(?!:n| |@|\n).*? \.$', data, flags=re.MULTILINE | re.DOTALL)
     list_sort = sorted(test_search, key=lambda x: (len(x.split(' ')[0]), x))
-
+    prefix_matches.insert(len(prefix_matches), "\n")
     result = prefix_matches + format_list_items(other_matches) + format_list_items(list_sort)
 
     with open("rpb-spatial_sorted.ttl", "w") as outfile:
